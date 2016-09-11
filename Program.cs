fb1f08a9fffa9c38398fc1e21a92ceeec1cb8198
caton <caton182@gmail.com>
2016-09-11 13:35:00 +0200

    nauka C#

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using test;
+
+namespace test {
+	class jakas {
+		public static void Main(){
+			jan ();
+			test2.jakas.jan ();
+		}
+
+		public static void jan(){
+			Console.WriteLine("klasa test metoda jan");
+	}
+}
+}
+namespace test2 {
+	class jakas {
+	//	public static void Main(){
+	//	}
+			public static void jan(){
+				Console.WriteLine("klasa test2 metoda jan");
+			}
+
+}
+	}
